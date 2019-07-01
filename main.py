b93f47db4cab7c23adeaf9058214c2a01adf9a79
Ilya Mikhailutca <io.mikhailutca@severstal.com>
2019-07-01 09:50:56 +0300

    oneX

---
+++ main.py
@@ -0,0 +1,52 @@
+import pprint
+dicts = dict()
+with open('Cook.txt', 'r', encoding='utf8') as f:
+    for line in f:
+        if line.strip() == '':
+            name = f.readline().strip()
+            dicts.setdefault(name, list())
+        else:
+            counts = line.strip()
+            count = 0
+            while count < int(counts):
+                stri = f.readline().strip().split('|')
+                info_cook = {"ingridient_name": stri[0], "quantity": stri[1], "measure": stri[2]}
+                dicts[name].append(info_cook)
+                count += 1
+print(dicts)
+
+
+
+cook_dict = dict()
+def get_shop_list_by_dishes(dishes, person_count):
+  for cook_name in dishes:
+    if cook_name in dicts.keys():
+        for items_list in dicts.keys():
+            if cook_name == items_list:
+                for item in dicts[items_list]:
+                    for key, value in item.items():
+                            if key == 'quantity':
+                                values = int(value)
+                                item[key] = values * 5         
+                    for key, value in item.items():
+                      if key == 'ingridient_name':
+                        name = value
+                        cook_dict.setdefault(value, dict())
+                      elif key == 'measure':
+                        cook_dict[name].setdefault(key,value.strip())
+                      elif key == 'quantity':
+                        cook_dict[name].setdefault(key,value)
+    else:
+      return('Нет блюда: ' + cook_name)
+  return cook_dict
+  
+
+
+dis = list()
+count_cook = int(input('\n\n Введите количество вводимых блюд: \n'))
+person = int(input('Введите количество персон: '))
+for i in range (count_cook):
+    x=input("Введите наименования блюда: \n")
+    dis.insert(i,x)
+    i+=1
+pprint.pprint(get_shop_list_by_dishes(dis, person))
